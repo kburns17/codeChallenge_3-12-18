e28632276fc6784c6cb752a5bd8b78750591d4c6
Kerry Burns <kerryalanburns@gmail.com>
2018-03-12 14:32:34 -0500

    almost step 6

--- client.js
+++ client.js
@@ -12,7 +12,6 @@ function appendDom() {
   $('#generate').on('click', addDiv);
   $('#generate').on('click', countDiv);
   $('#header').addClass('red');
-  
 
 }//end appendDom
 
@@ -21,12 +20,22 @@ function addDiv() {
   $('#header').append('<h1>Coding Challenge 3-12-18</h1>');
   $('#header').append('<button id="swap">Swap</button>')
   $('#header').append('<button id="delete">Delete</button>')
+  $('#swap').on('click', swapColor);
+
 
 } //end addDiv
-
 
 function countDiv() {
   divCounter++;
   console.log('in counter');
   $('#counter').text('div count: ' + divCounter)
+} // end countDiv
+
+function swapColor() {
+  console.log('in swap color');
+    if (  $('#header').addClass('red')) {
+      $('#header').addClass('yellow')
+    } else {
+    
+    }
 }
