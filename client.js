576bc2e58366fa6a986c7e94fa4eb311448cd1b0
Kerry Burns <kerryalanburns@gmail.com>
2018-03-12 13:33:45 -0500

    step 2 complete

---
+++ client.js
@@ -0,0 +1,13 @@
+
+$(document).ready(readyNow);
+
+function readyNow() {
+  $('#header').append('<h1>Coding Challenge 3-12-18</h1>');
+  appendDom();
+} //end readyNow
+
+function appendDom() {
+$('#generate').on('click');
+
+
+}
